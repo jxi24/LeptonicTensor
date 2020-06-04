f806efe10075a4aed9796ed1b1522a51ba63bcd9
Diego Lopez Gutierrez <42101059+DiegoLopezGutierrez@users.noreply.github.com>
2020-06-04 14:52:38 -0500

    Updates to get_vertex and amplitude. Added get_labels.

--- src/leptonic_tensor/feyn_rules.py
+++ src/leptonic_tensor/feyn_rules.py
@@ -5,6 +5,10 @@ class FeynRules:
         self.outgoing = outgoing_particles
         self.internal = internal_particles
         
+    def _get_labels(self):
+        labels = [i for i in range(1,len(self.incoming) + len(self.outgoing) + 1)]
+        return labels
+        
     def _get_pids(self, particles_list):
         pids = []
         for part in particles_list:
@@ -19,36 +23,44 @@ class FeynRules:
             particles_list.append(part)
         pids = self._get_pids(particles_list)
         vertex = self.model.vertex_map[tuple(pids)]
-        lorentz = vertex.lorentz[0].structure
-        coupling = vertex.couplings[(0,0)].value
-        return [lorentz, coupling]
+        if len(vertex.color) == 1:
+            lorentz = [ltz.structure for ltz in vertex.lorentz]
+            color = vertex.color
+            coupling = [vertex.couplings[(0,i)].value for i in range(len(lorentz))]
+            #lorentz = vertex.lorentz[0].structure
+            #coupling = vertex.couplings[(0,0)].value
+            return [lorentz, coupling, color]
+        else:
+            pass
     
     def _get_propagator(self, internal):
         propagator = self.model.propagator_map[internal.propagator]
+        
         return '(({}) / ({}))'.format(propagator.numerator, propagator.denominator)
     
-    def _amplitude(self):
+    def amplitude(self):
         incoming_wavefunctions = []
         outgoing_wavefunctions = []
         propagators = []
-        
+        labels = self._get_labels()
+        i = 0
         for part in self.incoming:
+            part.get_wavefunction(labels[i])
             incoming_wavefunctions.append(part.wavefunction[0])
+            i += 1
         for part in self.outgoing:
+            part.get_wavefunction(labels[i])
             outgoing_wavefunctions.append(part.wavefunction[1])
+            i += 1
         for part in self.internal:
             propagators.append(self._get_propagator(part))
         
         vertex_in = self._get_vertex(self.incoming, self.internal)
         vertex_out = self._get_vertex(self.outgoing, self.internal)
-        prefact = []
-        prefact.append(vertex_in[1])
-        prefact.append(vertex_out[1])
-        lorentz = []
-        lorentz.append(vertex_in[0])
-        lorentz.append(vertex_out[0])
+        prefact = vertex_in[1] + vertex_out[1] + vertex_in[2] + vertex_out[2]
+        lorentz = vertex_in[0] + vertex_out[0]
         
-        amp = [prefact[0], prefact[1], outgoing_wavefunctions[1], lorentz[1], outgoing_wavefunctions[0], propagators[0], incoming_wavefunctions[0], lorentz[0], incoming_wavefunctions[1]]
+        amp = prefact + [outgoing_wavefunctions[1], lorentz[1], outgoing_wavefunctions[0], propagators[0], incoming_wavefunctions[0], lorentz[0], incoming_wavefunctions[1]]
         Amp = '*'.join(amp)
         return Amp
     
