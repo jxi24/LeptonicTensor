2665f677c19b690f200fd674f38e7df96b61db99
Diego Lopez Gutierrez <42101059+DiegoLopezGutierrez@users.noreply.github.com>
2020-06-08 19:45:31 -0400

    Update Particle class and FeynRules class
    Updated the amplitude, get_vertex and get_propagator methods of FeynRules to account for the new class Particle and for only outgoing wavefunctions. Works fine for e+e- -> mu+mu-, but requires more work for more complicated processes.

--- src/leptonic_tensor/epem2mummup.py
+++ src/leptonic_tensor/epem2mummup.py
@@ -55,7 +55,11 @@ class Model:
 class Particle:
     def __init__(self, model, pid, momentum, incoming):
         self.momentum = momentum
-        self.info = model.particle_map[pid]
+        try:
+            self.info = model.particle_map[pid]
+        except:
+            # Particle is its own antiparticle.
+            self.info = model.particle_map[-pid]
         self.incoming = incoming
         self.model = model
 
@@ -63,13 +67,24 @@ class Particle:
         return Particle(self.model, -self.info.pid, -self.momentum, not self.incoming)
 
     def wavefunction(self):
+        '''
+        Return outgoing wavefunction of Particle, identified with
+        particle name and momentum label number.
+        '''
         if self.incoming:
             return self.anti().wavefunction()
         else:
-            if self.info.pid < 0:
-                return 'v(p_[{}, {}])'.format(self.info.name, self.momentum)
-            if self.info.pid > 0:
-                return 'u(p_[{}, {}])'.format(self.info.name, self.momentum)
+            if self.info.spin == 0:
+                return ''
+            if self.info.spin == 1:
+                if self.info.pid < 0:
+                    return 'v(p_[{}, {}])'.format(self.info.name, self.momentum)
+                if self.info.pid > 0:
+                    return 'ubar(p_[{}, {}])'.format(self.info.name, self.momentum)
+            if self.info.spin == 2:
+                return 'epsilon*(p_[{}, {}])'.format(self.info.name, self.momentum)
+            else:
+                pass
 
     def __str__(self):
         return 'Particle({}, {})'.format(self.momentum, self.info)
@@ -78,51 +93,72 @@ class Particle:
 def main():
     all_models = models.discover_models()
     model = Model('Models.SM_NLO', all_models)
-    ubar = Particle(model, -2, 3, False)
-    print(ubar.wavefunction())
-    u = ubar.anti()
-    print(u.wavefunction())
+    # ubar = Particle(model, -2, 3, False)
+    # print(ubar.wavefunction())
+    # u = ubar.anti()
+    # print(u.wavefunction())
 
 
     # Setup the incoming, outgoing, and internal particles
-    particles = model.particle_map
-    incoming_particles = [particles['e+'], particles['e-']]
-    outgoing_particles = [particles['mu+'], particles['mu-']]
-    internal_particles = [particles['a']]
+    # particles = model.particle_map
+    # incoming_particles = [particles[-11], particles[11]]
+    # outgoing_particles = [particles[-13], particles[13]]
+    # internal_particles = [particles[22]]
+    
+    elec = Particle(model, 11, 1, True)
+    antielec = Particle(model, -11, 2, True)
+    muon = Particle(model, 13, 3, False)
+    antimuon = Particle(model, -13, 4, False)
+    photon = Particle(model, 22, 0, False)
+    # print(elec.wavefunction())
+    # print(antielec.wavefunction())
+    # print(muon.wavefunction())
+    # print(antimuon.wavefunction())
+    
+    InP = [elec, antielec]
+    OutP = [muon, antimuon]
+    IntP = [photon]
 
-    pids1 = [particles['e+'].pid,
-             particles['e-'].pid,
-             particles['a'].pid]
-    pids1.sort()
-    print(pids1)
-    vertex1 = model.vertex_map[tuple(pids1)]
-    print(vertex1.lorentz[0].structure, vertex1.couplings[(0, 0)].value)
-    vertexA = vertex1.lorentz[0].structure.replace('3', 'mu').replace('2', 'e-').replace('1', 'e+')
+    # pids1 = [particles[-11].pid,
+    #          particles[11].pid,
+    #          particles[22].pid]
+    # pids1.sort()
+    # print(pids1)
+    # vertex1 = model.vertex_map[tuple(pids1)]
+    # print(vertex1.lorentz[0].structure, vertex1.couplings[(0, 0)].value)
+    # vertexA = vertex1.lorentz[0].structure.replace('3', 'mu').replace('2', 'e-').replace('1', 'e+')
 
-    pids2 = [particles['mu+'].pid,
-             particles['mu-'].pid,
-             particles['a'].pid]
-    pids2.sort()
-    print(pids2)
-    vertex2 = model.vertex_map[tuple(pids2)]
-    print(vertex2.lorentz[0].structure, vertex2.couplings[(0, 0)].value)
-    vertexB = vertex2.lorentz[0].structure.replace('3', 'nu').replace('2', 'mu-').replace('1', 'mu+')
+    # pids2 = [particles[-13].pid,
+    #          particles[13].pid,
+    #          particles[22].pid]
+    # pids2.sort()
+    # print(pids2)
+    # vertex2 = model.vertex_map[tuple(pids2)]
+    # print(vertex2.lorentz[0].structure, vertex2.couplings[(0, 0)].value)
+    # vertexB = vertex2.lorentz[0].structure.replace('3', 'nu').replace('2', 'mu-').replace('1', 'mu+')
 
-    propagator = model.propagator_map[particles['a'].propagator]
-    propagatorA = propagator.numerator.replace('1','mu').replace('2','nu') + '/' + propagator.denominator
-    print("({})/({})".format(propagator.numerator, propagator.denominator))
+    # propagator = model.propagator_map[particles[22].propagator]
+    # propagatorA = propagator.numerator.replace('1','mu').replace('2','nu') + '/' + propagator.denominator
+    # print("({})/({})".format(propagator.numerator, propagator.denominator))
 
-    amp = ['ubar(p2)', vertexA, 'v(p1)', propagatorA, 'vbar(p3)', vertexB, 'u(p4)']
-    print('*'.join(amp))
+    # amp = ['ubar(p2)', vertexA, 'v(p1)', propagatorA, 'vbar(p3)', vertexB, 'u(p4)']
+    # print('*'.join(amp))
 
-    amp1 = feyn_rules.FeynRules(model, incoming_particles, outgoing_particles, internal_particles)
-    #print('Incoming wavefunction for {}: {}'.format(particles['e-'].name, particles['e-'].wavefunction[0]))
-    print("\n")
-    print(amp1.amplitude())
-    print("\n")
-    #print(amp1._get_vertex(outgoing_particles, internal_particles))
-    amp2 = feyn_rules.FeynRules(model, incoming_particles, outgoing_particles, [particles['Z']])
-    print(amp2.amplitude())
+    # amp1 = feyn_rules.FeynRules(model, incoming_particles, outgoing_particles, internal_particles)
+    # print('Incoming wavefunction for {}: {}'.format(particles['e-'].name, particles['e-'].wavefunction[0]))
+    # print("\n")
+    # print(amp1.amplitude())
+    # print("\n")
+    # print(amp1._get_vertex(outgoing_particles, internal_particles))
+    # amp2 = feyn_rules.FeynRules(model, incoming_particles, outgoing_particles, [particles[23]])
+    # print(amp2.amplitude())
+    
+    Amp1 = feyn_rules.FeynRules(model, InP, OutP, IntP)
+    print(Amp1.amplitude())
+    
+    #Zboson = Particle(model, 23, 0, False)
+    #Amp2 = feyn_rules.FeynRules(model, InP, OutP, [Zboson])
+    #print(Amp2.amplitude())
 
 if __name__ == '__main__':
     main()
--- src/leptonic_tensor/feyn_rules.py
+++ src/leptonic_tensor/feyn_rules.py
@@ -1,19 +1,16 @@
 class FeynRules:
     def __init__(self, model, incoming_particles, outgoing_particles, internal_particles):
+        '''
+        Initiate object with Model instance and Particle instances.
+        '''
         self.model = model
         self.incoming = incoming_particles
         self.outgoing = outgoing_particles
         self.internal = internal_particles
         
-    def _get_labels(self):
-        labels = [i for i in range(1,len(self.incoming) + len(self.outgoing) + 1)]
-        return labels
-        
-    def _get_pids(self, particles_list):
-        pids = []
-        for part in particles_list:
-            pids.append(part.pid)
-        return pids
+    # def _get_labels(self):
+    #     labels = [i for i in range(1,len(self.incoming) + len(self.outgoing) + 1)]
+    #     return labels
         
     def _get_vertex(self, real_particles, virtual_particles):
         particles_list = []
@@ -21,10 +18,21 @@ class FeynRules:
             particles_list.append(part)
         for part in virtual_particles:
             particles_list.append(part)
-        pids = self._get_pids(particles_list)
+        pids = [part.info.pid for part in particles_list]
+        pids.sort()
         vertex = self.model.vertex_map[tuple(pids)]
         if len(vertex.color) == 1:
-            lorentz = [ltz.structure for ltz in vertex.lorentz]
+            lorentz = [] 
+            for ltz in vertex.lorentz:
+                if ('Gamma' in ltz.structure) & ('Gamma5' != ltz.structure):
+                    idx = ltz.structure.index('Gamma')
+                    l = [s for s in ltz.structure]
+                    # Gamma(1,2,3)
+                    l[idx+6] = str(virtual_particles[0].momentum)
+                    l[idx+8] = str(real_particles[0].momentum)
+                    l[idx+10] = str(real_particles[1].momentum)
+                    newl = ''.join(l)
+                    lorentz.append(newl)
             color = vertex.color
             coupling = [vertex.couplings[(0,i)].value for i in range(len(lorentz))]
             return [lorentz, coupling, color]
@@ -32,41 +40,98 @@ class FeynRules:
             pass
     
     def _get_propagator(self, internal):
-        propagator = self.model.propagator_map[internal.propagator]
-        num = propagator.numerator.replace('Mass(id)',str(internal.mass)).replace('Width(id)',str(internal.width)).replace('id',internal.name)
-        denom = propagator.denominator.replace('Mass(id)',str(internal.mass)).replace('Width(id)',str(internal.width)).replace('id',internal.name)
+        propagator = self.model.propagator_map[internal.info.propagator]
+        if 'Metric' in propagator.numerator:
+            idx = propagator.numerator.index('Metric')
+            l = [s for s in propagator.numerator]
+            # Metric(1, 2)
+            l[idx+7] = 'mu'
+            l[idx+10] = 'nu'
+            num = ''.join(l)
+        num = num.replace('Mass(id)',str(internal.info.mass)).replace('Width(id)',str(internal.info.width)).replace('id',internal.info.name)
+        denom = propagator.denominator.replace('Mass(id)',str(internal.info.mass)).replace('Width(id)',str(internal.info.width)).replace('id',internal.info.name).replace("'mu'", str(internal.momentum))
         return '(({}) / ({}))'.format(num, denom)
     
+    # def amplitude(self):
+    #     # All particles will be considered outgoing.
+    #     incoming_wavefunctions = []
+    #     outgoing_wavefunctions = []
+    #     propagators = []
+    #     labels = self._get_labels()
+    #     i = 0
+    #     for part in self.incoming:
+    #         part.get_wavefunction(labels[i])
+    #         incoming_wavefunctions.append(part.wavefunction[0])
+    #         i += 1
+    #     for part in self.outgoing:
+    #         part.get_wavefunction(labels[i])
+    #         outgoing_wavefunctions.append(part.wavefunction[1])
+    #         i += 1
+    #     for part in self.internal:
+    #         propagators.append(self._get_propagator(part))
+        
+    #     vertex_in = self._get_vertex(self.incoming, self.internal)
+    #     vertex_out = self._get_vertex(self.outgoing, self.internal)
+    #     #prefact = vertex_in[1] + vertex_out[1] + vertex_in[2] + vertex_out[2]
+    #     #lorentz = vertex_in[0] + vertex_out[0]
+    #     V_in = ''
+    #     if len(vertex_in[0]) > 1:
+    #             V_in += '('
+    #     for i in range(len(vertex_in[0])):
+    #         V_in += '(' + vertex_in[1][i] + ')' + '*' + vertex_in[2][0] + '*' + '(' + vertex_in[0][i] + ')'
+    #         if i != (len(vertex_in[0])-1):
+    #             V_in += '+'
+    #     if len(vertex_in[0]) > 1:
+    #         V_in += ')'
+    #     V_out = ''
+    #     if len(vertex_out[0]) > 1:
+    #             V_out += '('
+    #     for i in range(len(vertex_out[0])):
+    #         V_out += '(' + vertex_out[1][i] + ')' + '*' + vertex_out[2][0] + '*' + '(' + vertex_out[0][i] + ')'
+    #         if i != (len(vertex_out[0])-1):
+    #             V_out += '+'
+    #     if len(vertex_out[0]) > 1:
+    #             V_out += ')'
+        
+    #     amp = [outgoing_wavefunctions[1], V_out, outgoing_wavefunctions[0], propagators[0], incoming_wavefunctions[0], V_in, incoming_wavefunctions[1]]
+    #     Amp = '*'.join(amp)
+    #     return Amp
     def amplitude(self):
-        incoming_wavefunctions = []
-        outgoing_wavefunctions = []
-        propagators = []
-        labels = self._get_labels()
-        i = 0
+        wavefunctions = []
         for part in self.incoming:
-            part.get_wavefunction(labels[i])
-            incoming_wavefunctions.append(part.wavefunction[0])
-            i += 1
+            wf = part.wavefunction()
+            wavefunctions.append(wf)
         for part in self.outgoing:
-            part.get_wavefunction(labels[i])
-            outgoing_wavefunctions.append(part.wavefunction[1])
-            i += 1
+            wf = part.wavefunction()
+            wavefunctions.append(wf)
+            
+        propagators = []
         for part in self.internal:
             propagators.append(self._get_propagator(part))
-        
+            
         vertex_in = self._get_vertex(self.incoming, self.internal)
         vertex_out = self._get_vertex(self.outgoing, self.internal)
-        #prefact = vertex_in[1] + vertex_out[1] + vertex_in[2] + vertex_out[2]
-        #lorentz = vertex_in[0] + vertex_out[0]
+        
         V_in = ''
         for i in range(len(vertex_in[0])):
             V_in += '(' + vertex_in[1][i] + ')' + '*' + vertex_in[2][0] + '*' + '(' + vertex_in[0][i] + ')'
+            if i != (len(vertex_in[0])-1):
+                V_in += '+'
+        if len(vertex_in[0]) > 1:
+            V_in = '(' + V_in + ')'
+            
         V_out = ''
         for i in range(len(vertex_out[0])):
             V_out += '(' + vertex_out[1][i] + ')' + '*' + vertex_out[2][0] + '*' + '(' + vertex_out[0][i] + ')'
-        
-        amp = [outgoing_wavefunctions[1], V_out, outgoing_wavefunctions[0], propagators[0], incoming_wavefunctions[0], V_in, incoming_wavefunctions[1]]
-        Amp = '*'.join(amp)
-        return Amp
+            if i != (len(vertex_out[0])-1):
+                V_out += '+'
+        if len(vertex_out[0]) > 1:
+            V_out = '(' + V_out + ')'        
     
-            
+        inNames = [part.info.name for part in self.incoming]
+        outNames = [part.info.name for part in self.outgoing]
+        propNames = [part.info.name for part in self.internal]
+        amp = wavefunctions + propagators + [V_in, V_out]
+        amp = '*'.join(amp)
+        result = 'Amplitude for {} -> {} via {}. Amplitude format (wavefunctions, propagators, vertices):\n%s'.format(inNames, outNames, propNames) %amp
+        return result
