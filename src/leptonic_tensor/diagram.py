b995dc1e96e7bcea8bef49b49ab29eeb5dd7ee1a
Joshua Isaacson <isaacson@fnal.gov>
2020-11-24 14:04:14 -0600

    Implement a diagram generator using recursive merging

---
+++ src/leptonic_tensor/diagram.py
@@ -0,0 +1,223 @@
+import numpy as np
+import collections
+
+VERTICES = [[11, -11, 22], [13, -13, 22], [11, -12, 24], [-11, 12, -24],
+            [11, -11, 23], [13, -13, 23], [12, -12, 23], [24, -24, 22],
+            [24, -24, 23]]
+
+PARTMAP = {}
+
+
+def binary_conj(x, size):
+    vals = []
+    for y in bin(x)[2:].zfill(size):
+        if y == '1':
+            vals.append('0')
+        else:
+            vals.append('1')
+    return int(''.join(vals), 2)
+
+
+class Particle:
+    max_id = 0
+
+    def __init__(self, i, pid, direction):
+        self.id = i
+        self.pid = pid
+        self.direction = direction
+
+    def __str__(self):
+        sid = self.get_id()
+        return f'({sid}, {self.pid})'
+
+    def conjugate(self):
+        pid = self.pid
+        if self.pid not in (22, 23):
+            pid = -self.pid
+        return Particle(self.id, pid, -self.direction)
+
+    def __repr__(self):
+        return str(self)
+
+    def __lt__(self, other):
+        sid = self.get_id()
+        oid = other.get_id()
+        if sid < oid:
+            return True
+        elif oid < sid:
+            return False
+        return self.pid < other.pid
+
+    def get_id(self):
+        if self.id >= Particle.max_id:
+            return PARTMAP[self.id]
+        return self.id
+
+    def __eq__(self, other):
+        return (self.get_id() == other.get_id()
+                and abs(self.pid) == abs(other.pid))
+
+
+class Vertex:
+    def __init__(self, particles):
+        self.particles = particles
+
+    def __str__(self):
+        return "V("+', '.join([str(v.get_id()) for v in self.particles])+")"
+
+    def __repr__(self):
+        return str(self)
+
+    def __eq__(self, other):
+        sids = {}
+        spids = {}
+        if len(self.particles) != len(other.particles):
+            return False
+        for particle in self.particles:
+            if particle.get_id() not in sids:
+                sids[particle.get_id()] = 1
+                spids[abs(particle.pid)] = 1
+            else:
+                sids[particle.get_id()] += 1
+                spids[abs(particle.pid)] += 1
+
+        oids = {}
+        opids = {}
+        for particle in other.particles:
+            if particle.get_id() not in oids:
+                oids[particle.get_id()] = 1
+                opids[abs(particle.pid)] = 1
+            else:
+                oids[particle.get_id()] += 1
+                opids[abs(particle.pid)] += 1
+
+        return oids == sids and opids == spids
+
+
+class Propagator:
+    def __init__(self, particle):
+        self.particle = particle
+
+    def __str__(self):
+        return "P{}".format(self.particle)
+
+    def __repr__(self):
+        return str(self)
+
+    def __eq__(self, other):
+        return self.particle == other.particle
+
+
+class Diagram:
+    def __init__(self, external):
+        self.vertices = []
+        self.propagators = []
+        self.external = external
+        self.free = external
+
+    def copy(self):
+        diagram = Diagram(self.external.copy())
+        diagram.vertices = self.vertices.copy()
+        diagram.propagators = self.propagators.copy()
+        diagram.free = self.free.copy()
+        return diagram
+
+    def add_vertex(self, vertex):
+        vert = Vertex(vertex)
+        self.vertices.append(vert)
+
+    def add_propagator(self, propagator):
+        prop = Propagator(propagator)
+        self.propagators.append(prop)
+
+    def __eq__(self, other):
+        vert_bool = True
+        prop_bool = True
+        for svert in self.vertices:
+            if svert not in other.vertices:
+                return False
+        for sprop in self.propagators:
+            if sprop not in other.propagators:
+                return False
+        return (vert_bool and prop_bool
+                and self.external == other.external)
+
+    def __call__(self, momentums):
+        pass
+        # Create external currents:
+        # - J(1, mom1)
+        # - J(2, mom2)
+        # - J(4, mom3)
+        # etc.
+
+        # for vertex in self.vertices:
+        #     V(i, j, k) => V(i, j, k, ..., l, m)
+        #     Symmetry factors
+        #     J(k) = V(i, j, k, ..., l, m) * J(i) * J(j) * J(k) * ... * J(l)
+        #     Calculate momenutm for particle k (p(i) + p(j))
+        #     if k in self.propagators:
+        #         J(k) *= Prop(p(k))
+
+
+def AddVertex(diagram):
+    particles = diagram.free
+    diagrams = []
+    for i in range(len(particles)-1):
+        for j in range(i+1, len(particles)):
+            for v in VERTICES:
+                if particles[i].pid in v and particles[j].pid in v:
+                    new_particles = particles.copy()
+                    pids = list(set(v)
+                                - set((particles[i].pid, particles[j].pid)))
+                    if len(pids) > 1:
+                        continue
+                    pid = pids[0]
+                    new_id = particles[i].id+particles[j].id
+                    new_particles[i] = Particle(new_id, pid, -1)
+                    new_particles.remove(new_particles[j])
+                    new_diagram = diagram.copy()
+                    new_vertex = [particles[i], particles[j], new_particles[i]]
+                    new_diagram.add_vertex(new_vertex)
+                    new_diagram.add_propagator(new_particles[i])
+                    new_particles[i] = new_particles[i].conjugate()
+                    new_diagram.free = new_particles
+                    diagrams.append(new_diagram)
+
+    return diagrams
+
+
+def main():
+    particles = [Particle(1, -11, 1), Particle(2, 11, 1),
+                 Particle(4, 22, 1), Particle(8, 23, 1),
+                 Particle(16, 12, 1), Particle(32, -12, 1)]
+    # particles = [Particle(1, -11, 1), Particle(2, 11, 1),
+    #              Particle(4, 22, 1), Particle(8, 22, 1),
+    #              Particle(16, 23, 1)]
+    Particle.max_id = 1 << (len(particles)-1)
+    for i in range(Particle.max_id << 1):
+        PARTMAP[i] = binary_conj(i, len(particles))
+    print(PARTMAP)
+    diagrams = [Diagram(particles)]
+    final_diagrams = []
+    ndiagrams = 0
+    while len(diagrams) > 0:
+        tmp = AddVertex(diagrams.pop(0))
+        for config in tmp:
+            if len(config.free) != 2:
+                diagrams.append(config)
+            else:
+                if((config.free[0].pid in (22, 23)
+                    and config.free[0].pid == config.free[1].pid)
+                   or config.free[0].pid == -config.free[1].pid):
+                    config.propagators.pop()
+                    if config not in final_diagrams:
+                        final_diagrams.append(config)
+                        ndiagrams += 1
+        # print(particles_set)
+    print(ndiagrams)
+    for diagram in final_diagrams:
+        print(diagram.vertices, diagram.propagators)
+
+
+if __name__ == '__main__':
+    main()
