80ecb7a76cda0b86402e1a01e7bbe762603c2639
Diego Lopez Gutierrez <42101059+DiegoLopezGutierrez@users.noreply.github.com>
2021-04-06 12:38:04 -0500

    Addition of fermion propagators as well as Z and W boson propagators.

--- src/leptonic_tensor/propagator.py
+++ src/leptonic_tensor/propagator.py
@@ -6,7 +6,10 @@ class Propagator:
 
     def __init__(self, particle):
         self.particle = particle
-        if self.particle.is_vector:
+        # print("Is vector? ", self.particle.is_vector())
+        # print("Is fermion? ", self.particle.is_fermion())
+        # print("Is antifermion? ", self.particle.is_antifermion())
+        if self.particle.is_vector():
             if self.particle.pid == 22:
             # if self.particle.massless():
                 self.denominator = lambda p: (p[:, 0]*p[:, 0]-np.sum(p[:, 1:]*p[:, 1:], axis=-1))[:, np.newaxis, np.newaxis]
@@ -17,7 +20,17 @@ class Propagator:
                 self.denominator = lambda p: (p[:, 0]*p[:, 0]-np.sum(p[:, 1:]*p[:, 1:], axis=-1)-mass**2-1j*mass*width)[:, np.newaxis, np.newaxis]
                 self.numerator = lambda p: \
                     -1j*ls.METRIC_TENSOR[np.newaxis, ...] + 1j*np.einsum('bi,bj->bij', p, p)/mass**2
-
+            elif self.particle.pid == 23:
+                mass = 91.1876
+                width = 2.4952
+                self.denominator = lambda p: (p[:, 0]*p[:, 0]-np.sum(p[:, 1:]*p[:, 1:], axis=-1)-mass**2-1j*mass*width)[:, np.newaxis, np.newaxis]
+                self.numerator = lambda p: \
+                    -1j*ls.METRIC_TENSOR[np.newaxis, ...] + 1j*np.einsum('bi,bj->bij', p, p)/mass**2
+        elif self.particle.is_fermion() or self.particle.is_antifermion():
+            if self.particle.pid in [-13, -11, 11, 13]:
+                self.denominator = lambda p: (p[:, 0]*p[:, 0]-np.sum(p[:, 1:]*p[:, 1:], axis=-1))[:, np.newaxis, np.newaxis]
+                self.numerator = lambda p: \
+                    1j*np.einsum('bm, mij->bij', p, ls.GAMMA_MU)
     def __call__(self, p):
         return self.numerator(p)/self.denominator(p)
             
